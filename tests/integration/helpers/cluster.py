bec16c5c3448f88ccc7362eea12823c7c4999ead
Han Fei <hanfei19910905@gmail.com>
2026-03-26 17:39:12 +0100

    fix port conflict in test_database_hms and test_database_iceberg_lakekeeper_catalog

--- tests/integration/helpers/cluster.py
+++ tests/integration/helpers/cluster.py
@@ -694,7 +694,8 @@ class ClickHouseCluster:
         self.with_glue_catalog = False
         self.glue_catalog_port = 3000
         self.with_hms_catalog = False
-        self.hms_catalog_port = 9083
+        self._hms_catalog_port = None
+        self._lakekeeper_catalog_port = None
 
         self.with_azurite = False
         self.azurite_container = "azurite-container"
@@ -1011,6 +1012,20 @@ class ClickHouseCluster:
             return self._iceberg_rest_catalog_port
         self._iceberg_rest_catalog_port = self.port_pool.get_port()
         return self._iceberg_rest_catalog_port
+
+    @property
+    def hms_catalog_port(self):
+        if self._hms_catalog_port:
+            return self._hms_catalog_port
+        self._hms_catalog_port = self.port_pool.get_port()
+        return self._hms_catalog_port
+
+    @property
+    def lakekeeper_catalog_port(self):
+        if self._lakekeeper_catalog_port:
+            return self._lakekeeper_catalog_port
+        self._lakekeeper_catalog_port = self.port_pool.get_port()
+        return self._lakekeeper_catalog_port
 
     @property
     def redis_port(self):
@@ -1738,6 +1753,7 @@ class ClickHouseCluster:
 
     def setup_hms_catalog_cmd(self, instance, env_variables, docker_compose_yml_dir):
         self.with_hms_catalog = True
+        env_variables["HMS_CATALOG_PORT"] = str(self.hms_catalog_port)
         self.base_cmd.extend(
             [
                 "--file",
@@ -1764,6 +1780,8 @@ class ClickHouseCluster:
             file_name = extra_parameters["docker_compose_file_name"]
         if file_name == "docker_compose_iceberg_rest_catalog.yml":
             env_variables["ICEBERG_REST_CATALOG_PORT"] = str(self.iceberg_rest_catalog_port)
+        if file_name == "docker_compose_iceberg_lakekeeper_catalog.yml":
+            env_variables["LAKEKEEPER_CATALOG_PORT"] = str(self.lakekeeper_catalog_port)
         self.base_cmd.extend(
             [
                 "--file",
